dc6303e2b8abdc2a1ffb79332f83b37519ca5990
ADCeres <82452034+ADCeres@users.noreply.github.com>
2021-04-18 17:51:10 -0400

    Streamlined Javascript & HTML IDs

--- src/script_api.js
+++ src/script_api.js
@@ -2,49 +2,9 @@
 //the following sections updates the ID: #cur-day-time with the current information
 //the date/time if formatted: DAY, MONTH DATE, HOUR:MINUTE AM/PM
 
-//this function determines if the hour is AM or PM
-function defineAM_PM () {
-    let time = new Date;
-    let defineTime = time.getHours();
-    if (defineTime < 11) {
-        return "AM"
-    } else {
-        return "PM"
-    }
-}
-
-//this function reformats a leading 0 in minute indeces LESS THAN 10
-function fixMinutes () {
-    let time = new Date;
-    let minutes = time.getMinutes()
-
-    if (minutes < 10) {
-        return `0${minutes}`
-    } else {
-        return `${minutes}`
-    }
-}
 
 //this function generates remaining date data and returns fully formatted date display
-function showDayTime (date) {
 
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-
-    let currentDay = days[date.getDay()];
-    let currentMonth = months[date.getMonth()]
-    let currentDate = date.getDate()
-    let currentHour = hours[date.getHours()]
-    let currentMinute = fixMinutes()
-    let am_or_pm = defineAM_PM()
-
-    let formattedDate = `${currentDay}, ${currentMonth} ${currentDate}, ${currentHour}:${currentMinute}${am_or_pm}`
-    return formattedDate
-}
-
-let dateDisplay = document.querySelector("#cur-day-time");
-dateDisplay.innerHTML = showDayTime(new Date)
 
 
 //this section updates the Current Temperature & City through API
@@ -83,40 +43,7 @@ let currentLocation = document.querySelector("#current-location-button")
 currentLocation.addEventListener("click", getPosition)
 
 
-//this section updates the current temperature through API
-function getTemperature (response) {
-    let oldTemp = document.querySelector("#cur-temp");
-    let newTemp = response.data.main.temp;
-        newTemp =  newTemp.toFixed(0)
-        newTemp = `${newTemp}°F`
-    oldTemp.innerHTML = newTemp;
-}
 
-
-//the following section refines the functionality of the ID: #entry-line
-//the submission of a new city updates the ID: "#selected-city" 
-function updateCity(event) {
-    event.preventDefault ();
-    let newCity = document.querySelector("#entry-line").value;
-        newCity = newCity.trim()
-        newCity = newCity.charAt(0).toUpperCase() + newCity.slice(1)
-    let oldCity = document.querySelector("#selected-city");
-    document.querySelector("#entry-line").value = ""
-    oldCity.innerHTML = newCity
-        
-    //this piece retriggers the updating of the current date
-    dateDisplay = document.querySelector("#cur-day-time");
-    dateDisplay.innerHTML = showDayTime(new Date)
-        
-    //this piece launches updateTemp
-    let apiKey = `a20670b64f2243817bd352afb3a3d0b5`
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${apiKey}`;
-    axios.get(url).then(getTemperature)
-}
-
-//this is the Event Listener button for the Change City Button
-let changeCity = document.querySelector("#submit-button")
-changeCity.addEventListener("click", updateCity)
 
 
 //the function converts the Current Temperature into Fahrenheit (if it is in Celsius)
@@ -172,7 +99,187 @@ let buttonConverterC = document.querySelector("#celsius")
 buttonConverterC.addEventListener("click", confirmUnitF)
 
 //this is the Event Listener button for the Celsius to Fahrenheit Conversion process
-let buttonConverterF = document.querySelector("#fahrenheit")
+let buttonConverterF = document.querySelector("#fahrenheit");
 buttonConverterF.addEventListener("click", confirmUnitC)
 
 
+
+//This section of code updates all the weather stats for a user-entered city
+
+    function updateTemp (newTemp) {
+        let oldTemp = document.querySelector("#cur-temp");
+            newTemp =  newTemp.toFixed(0)
+            newTemp = `${newTemp}°F`
+        oldTemp.innerHTML = newTemp;
+    }
+
+    function updateWeather (newWeather) {
+        let oldWeather = document.querySelector("#cur-emoji-desc")
+        oldWeather.innerHTML = newWeather
+    }
+        
+    function updateEmoji (newWeather) {
+        let newEmoji = document.querySelector("#cur-emoji")     
+
+        switch (newWeather) {
+        case "Clouds":
+            newEmoji.innerHTML = "☁️";
+            break;
+        case "Rain":
+            newEmoji.innerHTML = "🌧";
+            break;
+        case "Sunny":
+            newEmoji.innerHTML = "☀️";
+            break;
+        case "Snow":
+            newEmoji.innerHTML = "❄️";
+            break;
+        case "Extreme":
+            newEmoji.innerHTML = "❗";
+            break;
+        case "Windy":
+            newEmoji.innerHTML = "💨";
+            break;
+        case "Clear":
+            newEmoji.innerHTML = "☀️";
+            break;
+        default:
+            newEmoji.innerHTML = "❗❗";
+            break;
+        }
+    }
+
+    function updateWind (newWind) { 
+        let oldWind = document.querySelector("#wind")
+        newWind = newWind.toFixed(0)
+        oldWind.innerHTML = `${newWind}mph`
+    }   
+
+    function updateHumidity (newHumidity) {
+        let oldHumidity = document.querySelector("#humidity")
+        oldHumidity.innerHTML = `${newHumidity}%`
+    }
+
+    function convertHour (hour) {
+        let hours = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        return hours[hour]
+    }
+
+    function convertMinutes (minute) {
+        if (minute < 10) {
+            return `0${minute}`
+        } else {
+            return `${minute}`
+        }
+    }
+    
+    function defineAM_PM (hour) {
+        if (hour < 11) {
+            return "AM"
+        } else {
+            return "PM"
+        }
+    }
+
+    function updateSunrise (newSunrise) {
+        let oldSunrise = document.querySelector("#sunrise")
+            newSunrise = newSunrise * 1000
+            dateSunrise = new Date(newSunrise)
+            hoursSunrise = dateSunrise.getHours()
+            convertedHour = convertHour(hoursSunrise)
+            minutesSunrise = dateSunrise.getMinutes()
+            convertedMinute = convertMinutes(minutesSunrise)
+            morningOrAfternoon = defineAM_PM(hoursSunrise)
+            fullSunrise = `${convertedHour}:${convertedMinute}${morningOrAfternoon}`
+            oldSunrise.innerHTML = fullSunrise
+    }
+
+    function updateSunset (newSunset) {
+        let oldSunset = document.querySelector("#sunset")
+            newSunset = newSunset * 1000
+            dateSunset = new Date(newSunset)
+            hoursSunset = dateSunset.getHours()
+            convertedHour = convertHour(hoursSunset)
+            minutesSunset = dateSunset.getMinutes()
+            convertedMinute = convertMinutes(minutesSunset)
+            morningOrAfternoon = defineAM_PM(hoursSunset)
+            fullSunset = `${convertedHour}:${convertedMinute}${morningOrAfternoon}`
+            oldSunset.innerHTML = fullSunset
+    }
+    
+    function getStats (response) {
+        let newTemp = response.data.main.temp;
+        let newWeather = response.data.weather[0].main
+        let newWind = response.data.wind.speed
+        let newHumidity = response.data.main.humidity
+        let newSunrise = response.data.sys.sunrise
+        let newSunset = response.data.sys.sunset
+            updateTemp(newTemp)
+            updateWeather(newWeather)
+            updateEmoji(newWeather)
+            updateWind(newWind)
+            updateHumidity(newHumidity)
+            updateSunrise(newSunrise)
+            updateSunset(newSunset)
+    }
+
+    function updateCity (newCity) {
+        newCity = newCity.trim()
+        newCity = newCity.charAt(0).toUpperCase() + newCity.slice(1)
+        let oldCity = document.querySelector("#selected-city");
+        document.querySelector("#entry-line").value = ""
+        oldCity.innerHTML = newCity
+    }
+    
+    function triggerApi(event) {
+        event.preventDefault ();
+    
+        //this piece stores the enter city and sends it to the updateCity function
+        let newCity = document.querySelector("#entry-line").value;
+        updateCity(newCity)
+
+        //this piece triggers pulling the API data to initiate other functions
+        let apiKey = `a20670b64f2243817bd352afb3a3d0b5`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${apiKey}`;
+        axios.get(url).then(getStats)
+        
+        //this piece retriggers the updating of the current date
+        dateDisplay = document.querySelector("#cur-day-time");
+        dateDisplay.innerHTML = showDayTime(new Date)
+    }   
+
+//this is the Event Listener button for the Change City Button
+let changeCity = document.querySelector("#submit-button")
+changeCity.addEventListener("click", triggerApi)
+
+
+    function initialCity (){
+        let newCity = "New York"
+            updateCity(newCity)
+        let apiKey = `a20670b64f2243817bd352afb3a3d0b5`
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${newCity}&units=imperial&appid=${apiKey}`;
+            axios.get(url).then(getStats)
+    }   
+
+    initialCity()
+
+    function showDayTime (date) {
+
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    
+        let currentDay = days[date.getDay()];
+        let currentMonth = months[date.getMonth()]
+        let currentDate = date.getDate()
+        let currentHour = date.getHours()
+            convertedHour = convertHour(currentHour)
+        let currentMinute = date.getMinutes()
+            convertedMinute = convertMinutes(currentMinute)
+        let am_or_pm = defineAM_PM(currentHour)
+
+        let formattedDate = `${currentDay}, ${currentMonth} ${currentDate}, ${convertedHour}:${currentMinute}${am_or_pm}`
+        return formattedDate
+    }
+
+    let dateDisplay = document.querySelector("#cur-day-time");
+    dateDisplay.innerHTML = showDayTime(new Date)
